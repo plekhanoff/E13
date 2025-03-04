1104f86d442e6af87da1ce9e4fe42d91de645d43
plekhanoff <138236460+plekhanoff@users.noreply.github.com>
2025-03-04 14:01:42 +0300

    manual editing

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,50 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const HtmlWebpackPlugin = require("html-webpack-plugin")
 const TerserWebpackPlugin = require("terser-webpack-plugin")
-const OptimizeCssAssetsWebpackPlugin = require("optimize-css-assets-webpack-plugin")
 const ESLintPlugin = require('eslint-webpack-plugin')
 const Stylelint = require('stylelint')
-const { hotLoader } = require('mini-css-extract-plugin/types/loader')
+const CssMinimizerWebpackPlugin = require("css-minimizer-webpack-plugin")
+const StylelintWebpackPlugin = require('stylelint-webpack-plugin')
+const path = require('path')
+const options = {
+    files: 'src/**/*.css',
+    fix: true,
+    failOnError: false,
+    failOnWarning: false,
+    quiet: false
+}
+//const { hotLoader } = require('mini-css-extract-plugin/types/loader')
 
 module.exports = {
     entry: './src/index.js',
     mode: 'development',
-    devtool: 'inline-source-map',
+    devtool: 'false',
     devServer: {
-        static: './dist',
+        static: {directory: path.join(__dirname, 'dist')},
         historyApiFallback: true,
-        contentBase: "./dist",
         open: true,
         hot: true,
         port: 8080
       },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js', '.css', '.scss', '.sass', '.pug', '.html']
+  },
     output: {
         filename: 'main.js',
-        mode: 'development',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
     },
     plugins: [new MiniCssExtractPlugin(),
-              new HtmlWebpackPlugin({title:"Development",}),
+              new HtmlWebpackPlugin({title:"Development",template: 'src/index.html'}),
               new TerserWebpackPlugin(),
-              new OptimizeCssAssetsWebpackPlugin(),
-              new ESLintPlugin,
-              new Stylelint(options),
+              new CssMinimizerWebpackPlugin(),
+              new ESLintPlugin(),
+              new StylelintWebpackPlugin(options),
             ] ,    
     
     optimization:{
         minimize: true,
-        minimizer: [ new TerserWebpackPlugin(), new OptimizeCssAssetsWebpackPlugin()]
+        minimizer: [ new TerserWebpackPlugin() ]
     },
     module: {
         rules: [
@@ -52,6 +64,16 @@ module.exports = {
             { test:/\.tsx?$/,
               use: 'ts-loader',
               exclude: /node_modules/,}
+            {
+            test: /\.m?js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+                }
+            }
     ]
   }
      
