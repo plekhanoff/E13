e290bb32ddbfc556d856a466517167d344e15eb8
plekhanoff <138236460+plekhanoff@users.noreply.github.com>
2025-03-04 14:07:18 +0300

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,34 @@
+import { fixupConfigRules } from "@eslint/compat";
 import globals from "globals";
-import tseslint from "typescript-eslint";
-import pluginVue from "eslint-plugin-vue";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
 
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  {files: ["**/*.{js,mjs,cjs,ts,vue}"]},
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
-  ...tseslint.configs.recommended,
-  ...pluginVue.configs["flat/essential"],
-  {files: ["**/*.vue"], languageOptions: {parserOptions: {parser: tseslint.parser}}},
-];
+export default [...fixupConfigRules(
+    compat.extends("eslint:recommended", "plugin:import/errors", "plugin:import/warnings"),
+), {
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+        },},   
+    parserOptions: {
+        ecmaVersion: 12,
+        sourceType: "module",
+    },
+    rules: {
+        'no-undef': 'error',
+    },
+    env: {
+       browser: true, 
+       es2021: true,
+    },
+}];
